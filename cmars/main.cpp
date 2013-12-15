1aea1418be4873e2db319369889dc1983d048a9c
lithium <code@hlidskialf.com>
2013-12-14 20:11:50 -0800

    Initial Commit

---
+++ cmars/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cmars
+//
+//  Created by Wiggins on 12/14/13.
+//  Copyright (c) 2013 Hlidskialf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
